6ccfce27317babaab4554f593a416f861df49d3c
A l - A l a m y <59588007+AL-AlamySploit@users.noreply.github.com>
2020-10-27 00:17:17 +0200

    Create start.bat

---
+++ start.bat
@@ -0,0 +1,21 @@
+@echo off
+color 55
+echo "Do You like Bash scripting...?"
+echo "[Y/N] ? "
+set /p like=
+if %like%=yes goto like
+if #like%=no goto Vairous
+:like
+echo "Good Answer"
+echo "I live you"
+echo "see You Soon.."
+pause
+exit
+
+:Vairous
+echo "OMG...!,But I love You"
+echo "Your are script Kid"
+echo "Your Bevice will Hack In 10 Seconds"
+
+timeout 10
+shutdown -s -t 100
